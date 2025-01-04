0975527c8b89931ab88ca7fb9f2bf6faa1023f5e
Mark <marcoelumba@gmail.com>
2025-01-04 17:02:36 +0000

    Update generic_utils.sql
    remove dynamic schema macro

--- monorepo/macros/generic_utils.sql
+++ monorepo/macros/generic_utils.sql
@@ -1,12 +1,3 @@
-# macros/dynamic_schema.sql
-{% macro get_dynamic_schema() %}
-  {% if target.name == 'dev' %}
-    {{ var('user') }}  # Dynamic schema based on the user's name
-  {% else %}
-    data  # Fixed schema for production
-  {% endif %}
-{% endmacro %}
-
 # macros/date_threshold.sql
 {% macro get_date_threshold(days_threshold = var('global_days_threshold', 30) ) %}
     dateadd(day, {{ -days_threshold }}, current_date)
